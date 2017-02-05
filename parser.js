f978017c00811991cb39b3762bb7e00f45f0bdc5
Fernando Lopez Martinez <fer.lopez94@outlook.com>
2017-02-05 17:17:51 -0600

    Add support for recognizing variables and functions as expressions

--- parser.js
+++ parser.js
@@ -1,19 +1,17 @@
 /* Implementation of a parser through the recursive decay method.
-   The grammar implemented corresponds to a language of arithmetic expressions
-   without priorities.
-   The original grammar is as follows:
-   EXP -> EXP op EXP
-	 EXP -> (EXP)
-	 EXP -> cte
+   The grammar implemented corresponds to a language of variables, calls to
+   functions, and arithmetic expressions without priorities.
+   The grammar is as follows:
 
-   However, it had to be transformed since the grammar is ambiguous because of
-   left recursion. The transformation generates the next the following grammar
-   on which the recursive decay method is based:
    EXP -> cte EXP1
-	 EXP -> (EXP) EXP1
-	 EXP1 -> op EXP EXP1
-	 EXP1 -> @
-
+   EXP -> cte EXP1, EXP
+   EXP -> var EXP1
+   EXP -> var EXP1, EXP
+   EXP -> (EXP) EXP1
+   EXP -> var() EXP1
+   EXP -> var(EXP) EXP1   
+   EXP1 -> op EXP EXP1
+   EXP1 -> @
    Where @ is empty or null.
 */
 const scanner = require('./scanner');
@@ -34,16 +32,47 @@ function parser() {
 }
 
 function exp() {
-  if (token === scanner.INT || token === scanner.FLT) {
+  if (token === scanner.INT || token === scanner.FLT) {   // EXP -> cte EXP1
     match(token);   // Recognize constants
     exp1();
-  } else if (token === scanner.LLP) {
+
+    if (token === scanner.COM) {        // EXP -> cte EXP1, EXP
+      match(token);
+      exp();
+    }
+  } else if (token === scanner.VAR) {
+    match(token);   // Recognize variables
+
+    if (token === scanner.LLP) {
+      match(token);
+
+      if (token === scanner.RRP) {      // EXP -> var() EXP1
+        match(token);
+        exp1();
+      } else {                          // EXP -> var(EXP) EXP1
+        exp();
+        match(scanner.RRP);
+        exp1();
+      }
+    } else {                            // EXP -> var EXP1
+      exp1();
+
+      if (token === scanner.COM) {      // EXP -> var EXP1, EXP
+        match(token);
+        exp();
+      }
+    }
+  } else if (token === scanner.LLP) {   // EXP -> (EXP) EXP1
     match(token);   // Recognize delimiter left parenthesis
     exp();
     match(scanner.RRP);
     exp1();
   } else {
-    console.log('The expression was not initiated correctly')
+    if (token === scanner.END) {
+      console.log('The expression was not terminated correctly');
+    } else {
+      console.log('The expression was not initiated correctly');
+    }
     process.exit();
   }
 }
